1eaaa704c0478faa7b7f20658f2921c854f5bc53
Ekrem Bayram <37544619+ekrem007@users.noreply.github.com>
2022-03-03 23:24:03 +0300

    Add enemy walk to player Add enemy stop walking on defined attack distance

--- Assets/IK/Scripts/Enemy.cs
+++ Assets/IK/Scripts/Enemy.cs
@@ -4,12 +4,15 @@ public class Enemy : MonoBehaviour
 {
     ARP.APR.Scripts.APRController APR;
     ConfigurableJoint rootJoint;
+    Rigidbody rootRB;
     AutoAim player;
+    public float playerAttackDistance = 2;
 
     void Awake()
     {
         APR = this.transform.root.GetComponent<ARP.APR.Scripts.APRController>();
         rootJoint = APR.Root.GetComponent<ConfigurableJoint>();
+        rootRB = APR.Root.GetComponent<Rigidbody>();
         player = GameObject.FindObjectOfType<AutoAim>();
     }
 
@@ -18,15 +21,28 @@ public class Enemy : MonoBehaviour
         if (player)
         {
             rootJoint.targetRotation = Quaternion.Inverse(Quaternion.LookRotation(rootJoint.transform.position - player.transform.position));
+
+            if (Vector3.Distance(this.transform.position, player.transform.position) > playerAttackDistance)
+            {
+                Vector3 direction = APR.Root.transform.forward;
+                direction.y = 0f;
+                rootRB.velocity = Vector3.Lerp(rootRB.velocity, (direction * APR.moveSpeed) + new Vector3(0, rootRB.velocity.y, 0), Time.fixedDeltaTime * 10);
+            }
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision collision)
     {
-        Bullet bullet = other.GetComponent<Bullet>();
+        Bullet bullet = collision.transform.GetComponent<Bullet>();
         if (bullet)
         {
             Destroy(this.transform.root.gameObject);
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, playerAttackDistance);
     }
 }
