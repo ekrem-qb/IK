24b38103f90a367f7bbfe0961219ea6a09a750e5
Ekrem Bayram <37544619+ekrem007@users.noreply.github.com>
2022-03-29 21:19:33 +0300

    Made sliding platform transition using joint
    Made Hub transition using position changing

--- Assets/IK/Scripts/Hub.cs
+++ Assets/IK/Scripts/Hub.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class Hub : Slidable
+public class Hub : MonoBehaviour
 {
     [Serializable]
     public struct ConveyorDirection
@@ -9,19 +9,45 @@ public class Hub : Slidable
         public Vector3 on, off;
     }
 
+    public Toggler toggler;
     public Conveyor conveyor;
     public ConveyorDirection conveyorDirection;
+    public Transform movingPart;
+    public Vector3 movingPartToggleOffset;
+    public float transitionSpeed = 15;
+    private Vector3 _originalPosition;
+    private Vector3 _targetPosition;
 
-    internal override void OnSwitchToggle(bool isOn)
+    private void Awake()
     {
-        base.OnSwitchToggle(isOn);
+        toggler.Toggle += OnSwitchToggle;
+        _originalPosition = movingPart.localPosition;
+    }
+
+    private void OnSwitchToggle(bool isOn)
+    {
+        this.enabled = true;
         if (isOn)
         {
             conveyor.direction = conveyorDirection.on;
+            _targetPosition = _originalPosition + movingPartToggleOffset;
         }
         else
         {
+            _targetPosition = _originalPosition;
             conveyor.direction = conveyorDirection.off;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector3.Distance(movingPart.localPosition, _targetPosition) > 0.05f)
+        {
+            movingPart.localPosition = Vector3.Lerp(movingPart.localPosition, _targetPosition, transitionSpeed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            this.enabled = false;
         }
     }
 }
--- Assets/IK/Scripts/Slidable.cs
+++ Assets/IK/Scripts/Slidable.cs
@@ -5,16 +5,63 @@ public class Slidable : MonoBehaviour
     public Toggler toggler;
     public ConfigurableJoint movingPart;
     public Vector3 movingPartToggleOffset;
+    public float transitionSpeed = 15;
+    private const float mass = 10000;
 
     private void Awake()
     {
         toggler.Toggle += OnSwitchToggle;
-        SoftJointLimit limit = movingPart.linearLimit;
-        limit.limit = Mathf.Max(movingPartToggleOffset.x, movingPartToggleOffset.y, movingPartToggleOffset.z);
+        Rigidbody rb = movingPart.GetComponent<Rigidbody>();
+        rb.mass = mass;
+        rb.drag = mass;
+        rb.useGravity = false;
+        JointDrive drive = new JointDrive
+        {
+            positionSpring = mass * 100 * transitionSpeed,
+            positionDamper = mass,
+            maximumForce = mass * 100 * transitionSpeed,
+        };
+        if (movingPartToggleOffset.x > 0)
+        {
+            movingPart.xMotion = ConfigurableJointMotion.Limited;
+            movingPart.xDrive = drive;
+        }
+        else
+        {
+            movingPart.xMotion = ConfigurableJointMotion.Locked;
+        }
+
+        if (movingPartToggleOffset.y > 0)
+        {
+            movingPart.yMotion = ConfigurableJointMotion.Limited;
+            movingPart.yDrive = drive;
+        }
+        else
+        {
+            movingPart.yMotion = ConfigurableJointMotion.Locked;
+        }
+
+        if (movingPartToggleOffset.z > 0)
+        {
+            movingPart.zMotion = ConfigurableJointMotion.Limited;
+            movingPart.zDrive = drive;
+        }
+        else
+        {
+            movingPart.zMotion = ConfigurableJointMotion.Locked;
+        }
+
+        movingPart.angularXMotion = ConfigurableJointMotion.Locked;
+        movingPart.angularYMotion = ConfigurableJointMotion.Locked;
+        movingPart.angularZMotion = ConfigurableJointMotion.Locked;
+        SoftJointLimit limit = new SoftJointLimit
+        {
+            limit = Mathf.Max(Mathf.Abs(movingPartToggleOffset.x), Mathf.Abs(movingPartToggleOffset.y), Mathf.Abs(movingPartToggleOffset.z))
+        };
         movingPart.linearLimit = limit;
     }
 
-    internal virtual void OnSwitchToggle(bool isOn)
+    private void OnSwitchToggle(bool isOn)
     {
         if (isOn)
         {
